3e0e4ab24de0bb3bf2bcc8a11df4127a3367aff4
jpmehedi <mehedihasan.cse.jp@gmail.com>
2025-03-26 16:13:43 +0900

    Achivement page design

---
+++ lib/achievement_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AchievementScreen extends StatelessWidget {
+  const AchievementScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0077FF),
+      appBar: AppBar(
+        title: Text("Our Milestones".toUpperCase(),
+        style: const TextStyle(color: Colors.white, letterSpacing: 2.0),
+       ),
+        backgroundColor: const Color(0xFF0077FF),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 35),
+          child: Column(
+            children: [
+              Text("What sets our studio apart for your projects?",
+                style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30,),
+              AchievementCard(
+                title: "8300+",
+                subTitle: "Figma ipsum component variant main layer. Hand.",
+              ),
+              SizedBox(height: 30,),
+              AchievementCard(
+                title: "100%",
+                subTitle: "Figma ipsum component variant main layer. Union.",
+              ),
+              SizedBox(height: 30,),
+              AchievementCard(
+                title: "3.5K",
+                subTitle: "Figma ipsum component variant main layer.",
+              ),
+              SizedBox(height: 30,),
+              AchievementCard(
+                title: "240+",
+                subTitle: "Figma ipsum component variant main layer.",
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AchievementCard extends StatelessWidget {
+  final String? title;
+  final String? subTitle;
+  const AchievementCard({
+    super.key, this.title, this.subTitle
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 171,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15)
+      ),
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("$title", style: const TextStyle(color: Colors.black, fontSize: 50, fontWeight: FontWeight.bold),),
+            Text("$subTitle",
+                style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.normal),
+              textAlign: TextAlign.center,
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/services_screen.dart
+++ lib/services_screen.dart
@@ -1,56 +1,129 @@
 
 import 'package:flutter/material.dart';
 
-class ServicesScreen extends StatelessWidget {
+class ServicesScreen extends StatefulWidget {
   const ServicesScreen({super.key});
 
   @override
+  State<ServicesScreen> createState() => _ServicesScreenState();
+}
+
+class _ServicesScreenState extends State<ServicesScreen> {
+  @override
   Widget build(BuildContext context) {
+    final PageController _controller = PageController(initialPage: 0);
+    final List<Map<String, dynamic>> data = [
+      {
+        "title": "Website / App Design UX / UI Design",
+        "subTitle": "Creating Engaging Digital Experiences for Websites and Apps through UX/UI Design"
+      },
+      {
+        "title": "Strategic Marketing and Creative Content",
+        "subTitle": "Elevating Brands and Engagement through Strategic Marketing and Creative Content"
+      },
+      {
+        "title": "Multivendor eCommerce Website Solutions",
+        "subTitle": "Unlocking the World of Multivendor eCommerce Websites"
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text("Service"),),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
-            GestureDetector(
-              onTap: (){
-                print("Click");
-              },
-              child: Container(
-                width: double.infinity,
-                height: 366,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF0077FF),
-                  borderRadius: BorderRadius.circular(10)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: (){
+                      _controller.previousPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeInOut
+                      );
+                    },
+                    icon: Icon(Icons.arrow_left)
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 30, right: 9, top: 30, bottom: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset("assets/logo/web.png"),
-                      const SizedBox(height: 10,),
-                      const Text("Website / App Design UX / UI Design", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
-                      const SizedBox(height: 10,),
-                      const Text(
-                        "Creating Engaging Digital Experiences for Websites and Apps through UX/UI Design",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                      const SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Text("Learn more",  style: TextStyle(color: Colors.white, fontSize: 18),),
-                          const SizedBox(width: 8,),
-                          Image.asset("assets/logo/arrow_right.png"),
-                        ],
-                      ),
-                    ],
-                  ),
+                IconButton(
+                    onPressed: (){
+                      _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeInOut
+                      );
+                    },
+                    icon: Icon(Icons.arrow_right)
                 ),
+              ],
+            ),
+
+            SizedBox(
+              height: 366,
+              child: PageView.builder(
+                itemCount: data.length,
+                controller: _controller,
+                scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                    return ServiceCard(
+                      title: "${data[index]["title"]}",
+                      subTitle: "${data[index]["subTitle"]}",
+                    );
+                  }
               ),
             )
+
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ServiceCard extends StatelessWidget {
+
+  final String? title;
+  final String? subTitle;
+  const ServiceCard({
+    super.key, this.title, this.subTitle
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        print("Click");
+      },
+      child: Container(
+        width: double.infinity,
+        height: 366,
+        decoration: BoxDecoration(
+          color: const Color(0xFF0077FF),
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 9, top: 30, bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset("assets/logo/web.png"),
+              const SizedBox(height: 10,),
+              Text("$title", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
+              const SizedBox(height: 10,),
+              Text(
+                "$subTitle",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("Learn more",  style: TextStyle(color: Colors.white, fontSize: 18),),
+                  const SizedBox(width: 8,),
+                  Image.asset("assets/logo/arrow_right.png"),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
